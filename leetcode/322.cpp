c23114871eecee6e27d74bb9b21b0cc1e1e896b8
DaLama007 <marwan.lamesch@gmail.com>
2026-03-30 15:40:17 +0200

    Solve 322. Coin change. An interesting problem about dp. This problem required me to look at the dp table like a storage for the min count per amount. This wasn't too easy and I had to ask AI to understand that I had to calculate the amount of coins for every amount there is till amount.

---
+++ leetcode/322.cpp
@@ -0,0 +1,20 @@
+#include <climits>
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i < amount+1; i++) {
+          for (int j = 0; j < coins.size(); j++) {
+            int coin = coins[j];
+            if (i-coin>=0 && dp[i-coin]!=INT_MAX) {
+              dp[i] = min(dp[i],1+dp[i-coin]);
+            }
+          }
+        }
+        if (dp[amount] == INT_MAX) {
+          return -1;
+        }
+        return dp[amount];
+    }
+};
