83771ab3a8a6b9a275eff29a47b23ac378667db0
DaLama007 <marwan.lamesch@gmail.com>
2026-06-30 23:24:26 +0200

    Complete problem C after contest. Way easier than I thought.

--- cf1107/c.cpp
+++ cf1107/c.cpp
@@ -7,26 +7,9 @@ int main (int argc, char *argv[]) {
     int l; cin >> l;
     string num; cin >> num;
 
-    int bsum = 0;
-    int curr = 1;
-    for (int i = l-1; i >= 0; i--) {
-      if(num[i] == '1'){ bsum += curr;
-      cout << "ADD "<< curr << " for idx: " << i << "\n";}
-      curr = 2 * curr;
-    }
-    cout << "BSUM: " << bsum << "\n";
-    int otherSum = 0;
-    int currMult = -1;
-    while(bsum != 0){
-      int mod = bsum%10;
-      otherSum += mod*currMult;
-      currMult = -currMult;
-      bsum = bsum/10;
-    }
-    if(otherSum == 0) cout << "1" << "\n";
-    else cout << "2" << "\n";
-
-
+    int count =0;
+    for(int i =0 ; i < l-1; i++ ) if(num[i] != num[i+1]) count++;
+    cout<< (count==1 ? 2 : 1) << "\n";
   }
   return 0;
 }
